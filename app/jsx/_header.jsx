28cc6701e2cf05468de5eee5ad99231a75d3e462
Mario Tema <Mario.Tema@gmx.de>
2016-09-27 08:12:21 +0200

    BugFix update number on unread messages (css)

--- app/jsx/_header.jsx
+++ app/jsx/_header.jsx
@@ -1,8 +1,7 @@
 var MessageNotificationHeader = React.createClass({
     render: function () {
         return <div className="row message-motification-header">
-            <img className="pull-left" src="img/udg-logo.png" border="2"/>
-            <h6 className="pull-right">designedbymario</h6>
+            <h2 className="logo"><bold>Rockwell Software</bold></h2>
         </div>
     }
 });
